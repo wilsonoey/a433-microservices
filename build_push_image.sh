6d24ca5cd1d852a582602fcde20c5891b63523f5
Wilson Jonathan Oey <61810752+wilsonoey60@users.noreply.github.com>
2022-12-24 16:40:00 +0800

    Update build_push_image.sh

--- build_push_image.sh
+++ build_push_image.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-# build image dengan tag item-app:v1
+# build image menggunakan tag item-app:v1
 docker build . -t item-app:v1
-# menampilkan list image
+# menampilkan daftar image
 docker image ls
 # membuat tag baru dari item-app:v1 ghcr.io/berviantoleo/a433-microservices:v1
 docker tag item-app:v1 ghcr.io/wilsonoey60/a433-microservices:v1
