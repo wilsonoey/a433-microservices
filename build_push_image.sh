9a74e89d9cda806d4ffdb5b95c53eb6e5d6afaf5
Wilson Jonathan Oey <61810752+wilsonoey60@users.noreply.github.com>
2024-06-18 16:27:51 +0800

    Update build_push_image.sh

--- build_push_image.sh
+++ build_push_image.sh
@@ -1,13 +1,38 @@
-git clone https://github.com/GoogleCloudPlatform/python-docs-samples.git
+export PROJECT_ID=$(gcloud config get-value project)
+export BUCKET_NAME=$(gcloud config get-value project)
 
-cd python-docs-samples/appengine/standard_python3/hello_world
+gcloud config set compute/region REGION
 
-dev_appserver.py app.yaml
+mkdir gcf_hello_world
 
-cd python-docs-samples/appengine/standard_python3/hello_world
+cd gcf_hello_world
 
-nano main.py
+cat > index.js <<EOF_CP
+/**
+* Background Cloud Function to be triggered by Pub/Sub.
+* This function is exported by index.js, and executed when
+* the trigger topic receives a message.
+*
+* @param {object} data The event payload.
+* @param {object} context The event metadata.
+*/
+exports.helloWorld = (data, context) => {
+const pubSubMessage = data;
+const name = pubSubMessage.data ? Buffer.from(pubSubMessage.data, 'base64').toString() : "Hello World";
+console.log(`My Cloud Function: ${name}`);
+};
 
-gcloud app deploy
+gcloud services disable cloudfunctions.googleapis.com
 
-gcloud app browse
+gcloud services enable cloudfunctions.googleapis.com
+
+gcloud projects add-iam-policy-binding [PROJECT_ID] \
+--member="serviceAccount:[PROJECT_ID]@appspot.gserviceaccount.com" \
+--role="roles/artifactregistry.reader"
+
+gcloud functions deploy helloWorld \
+  --stage-bucket [BUCKET_NAME] \
+  --trigger-topic hello_world \
+  --runtime nodejs20
+
+gcloud functions describe helloWorld
