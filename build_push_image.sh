76a7a0b7995e29b623239b0313acd1a47212a3b6
Wilson Jonathan Oey <61810752+wilsonoey60@users.noreply.github.com>
2024-06-18 18:54:34 +0800

    Update build_push_image.sh

--- build_push_image.sh
+++ build_push_image.sh
@@ -1,41 +1,15 @@
-export PROJECT_ID=$(gcloud config get-value project)
-export BUCKET_NAME=$(gcloud config get-value project)
+export ZONE=us-central1
 
-gcloud config set compute/region REGION
+gcloud config set compute/zone ${ZONE}
 
-mkdir gcf_hello_world
+gcloud container clusters create --machine-type=e2-medium --zone=${ZONE} lab-cluster
 
-cd gcf_hello_world
+gcloud container clusters get-credentials lab-cluster
 
-cat <<EOF_END > index.js 
-/**
-* Background Cloud Function to be triggered by Pub/Sub.
-* This function is exported by index.js, and executed when
-* the trigger topic receives a message.
-*
-* @param {object} data The event payload.
-* @param {object} context The event metadata.
-*/
-exports.helloWorld = (data, context) => {
-const pubSubMessage = data;
-const name = pubSubMessage.data ? Buffer.from(pubSubMessage.data, 'base64').toString() : "Hello World";
-console.log(`My Cloud Function: ${name}`);
-};
-EOF_END
+kubectl create deployment hello-server --image=gcr.io/google-samples/hello-app:1.0
 
-gsutil mb -p ${PROJECT_ID} gs://${BUCKET_NAME}
+kubectl expose deployment hello-server --type=LoadBalancer --port 8080
 
-gcloud services disable cloudfunctions.googleapis.com
+sleep 70
 
-gcloud services enable cloudfunctions.googleapis.com
-
-gcloud projects add-iam-policy-binding ${PROJECT_ID} \
---member="serviceAccount:${PROJECT_ID}@appspot.gserviceaccount.com" \
---role="roles/artifactregistry.reader"
-
-gcloud functions deploy helloWorld \
-  --stage-bucket ${BUCKET_NAME} \
-  --trigger-topic hello_world \
-  --runtime nodejs20
-
-gcloud functions describe helloWorld
+gcloud container clusters delete lab-cluster
