be23ed08e6960ccf4e5c3b5d1e21b0c57b885e6d
Wilson Jonathan Oey <61810752+wilsonoey60@users.noreply.github.com>
2024-06-19 18:55:19 +0800

    Update build_push_image.sh

--- build_push_image.sh
+++ build_push_image.sh
@@ -1,28 +1,12 @@
-gcloud auth list
-gcloud config list project
-gcloud services enable \
-  compute.googleapis.com \
-  iam.googleapis.com \
-  iamcredentials.googleapis.com \
-  monitoring.googleapis.com \
-  logging.googleapis.com \
-  notebooks.googleapis.com \
-  aiplatform.googleapis.com \
-  bigquery.googleapis.com \
-  artifactregistry.googleapis.com \
-  cloudbuild.googleapis.com \
-  container.googleapis.com
-SERVICE_ACCOUNT_ID=vertex-custom-training-sa
-gcloud iam service-accounts create $SERVICE_ACCOUNT_ID  \
-    --description="A custom service account for Vertex custom training with Tensorboard" \
-    --display-name="Vertex AI Custom Training"
-PROJECT_ID=$(gcloud config get-value core/project)
-gcloud projects add-iam-policy-binding $PROJECT_ID \
-    --member=serviceAccount:$SERVICE_ACCOUNT_ID@$PROJECT_ID.iam.gserviceaccount.com \
-    --role="roles/storage.admin"
-gcloud projects add-iam-policy-binding $PROJECT_ID \
-    --member=serviceAccount:$SERVICE_ACCOUNT_ID@$PROJECT_ID.iam.gserviceaccount.com \
-    --role="roles/bigquery.admin"
-gcloud projects add-iam-policy-binding $PROJECT_ID \
-    --member=serviceAccount:$SERVICE_ACCOUNT_ID@$PROJECT_ID.iam.gserviceaccount.com \
-    --role="roles/aiplatform.user"
+gcloud services disable dataflow.googleapis.com
+gcloud services enable dataflow.googleapis.com
+bq mk taxirides
+bq mk \
+--time_partitioning_field timestamp \
+--schema ride_id:string,point_idx:integer,latitude:float,longitude:float,\
+timestamp:timestamp,meter_reading:float,meter_increment:float,ride_status:string,\
+passenger_count:integer -t taxirides.realtime
+
+
+export BUCKET_NAME=$(gcloud config get-value project)
+gsutil mb gs://$BUCKET_NAME/
