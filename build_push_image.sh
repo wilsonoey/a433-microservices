29c0573feceb1afd5087b4d9566902b44fe7fc07
Wilson Jonathan Oey <61810752+wilsonoey60@users.noreply.github.com>
2024-06-20 07:48:53 +0800

    Update build_push_image.sh

--- build_push_image.sh
+++ build_push_image.sh
@@ -1,36 +1,19 @@
-export PROJECT_ID=qwiklabs-gcp-02-a4f0c744876d
-
-export BUCKET_NAME=$PROJECT_ID
-
-gcloud auth list
-
-gcloud config list project
-
-gcloud config set compute/region us-east4
-
-gcloud storage buckets create gs://qwiklabs-gcp-02-a4f0c744876d-bucket --project=$PROJECT_ID --location=us
-
-docker run -it -e DEVSHELL_PROJECT_ID=$DEVSHELL_PROJECT_ID python:3.9 /bin/bash
-
-
-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-
-
-
-pip install 'apache-beam[gcp]'==2.42.0
-
-python -m apache_beam.examples.wordcount --output OUTPUT_FILE
-
-cat $(ls)
-
-
-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-
-BUCKET=gs://<bucket name provided earlier>
-
-python -m apache_beam.examples.wordcount --project $DEVSHELL_PROJECT_ID \
-  --runner DataflowRunner \
-  --staging_location $BUCKET/staging \
-  --temp_location $BUCKET/temp \
-  --output $BUCKET/results/output \
-  --region us-central1
+export ZONE=us-east4-b
+export REGION=${ZONE::-2}
+gcloud dataproc clusters create example-cluster \
+--region=$REGION \
+--zone=$ZONE \
+--master-machine-type=e2-standard-2 \
+--master-boot-disk-size=50GB \
+--num-workers=2 \
+--worker-machine-type=e2-standard-2 \
+--worker-boot-disk-size=50GB
+gcloud dataproc jobs submit spark \
+--region=$REGION \
+--cluster=example-cluster \
+--class=org.apache.spark.examples.SparkPi \
+--jars=file:///usr/lib/spark/examples/jars/spark-examples.jar \
+-- 1000
+gcloud dataproc clusters update example-cluster \
+--num-workers=4 \
+--region=$REGION
