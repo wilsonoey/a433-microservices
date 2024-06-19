c8a0539387c874d23f1b54345cdf2c74320c60dc
Wilson Jonathan Oey <61810752+wilsonoey60@users.noreply.github.com>
2024-06-19 18:37:49 +0800

    Update build_push_image.sh

--- build_push_image.sh
+++ build_push_image.sh
@@ -1,25 +1,28 @@
-export YOURBUCKETNAME=1dntmmdmgdhmghdmgmdmmdmgdmghmghmdmdgmdg
-
-export REGION=us-central1
-
-gsutil mb gs://${YOURBUCKETNAME}
-
-curl https://upload.wikimedia.org/wikipedia/commons/thumb/a/a4/Ada_Lovelace_portrait.jpg/800px-Ada_Lovelace_portrait.jpg --output ada.jpg
-
-gsutil cp ada.jpg gs://${YOURBUCKETNAME}
-
-rm ada.jpg
-
-gsutil cp -r gs://${YOURBUCKETNAME}/ada.jpg .
-
-gsutil cp gs://${YOURBUCKETNAME}/ada.jpg gs://${YOURBUCKETNAME}/image-folder/
-
-gsutil ls gs://${YOURBUCKETNAME}
-
-gsutil ls -l gs://${YOURBUCKETNAME}/ada.jpg
-
-gsutil acl ch -u AllUsers:R gs://${YOURBUCKETNAME}/ada.jpg
-
-gsutil acl ch -d AllUsers gs://${YOURBUCKETNAME}/ada.jpg
-
-gsutil rm gs://${YOURBUCKETNAME}/ada.jpg
+gcloud auth list
+gcloud config list project
+gcloud services enable \
+  compute.googleapis.com \
+  iam.googleapis.com \
+  iamcredentials.googleapis.com \
+  monitoring.googleapis.com \
+  logging.googleapis.com \
+  notebooks.googleapis.com \
+  aiplatform.googleapis.com \
+  bigquery.googleapis.com \
+  artifactregistry.googleapis.com \
+  cloudbuild.googleapis.com \
+  container.googleapis.com
+SERVICE_ACCOUNT_ID=vertex-custom-training-sa
+gcloud iam service-accounts create $SERVICE_ACCOUNT_ID  \
+    --description="A custom service account for Vertex custom training with Tensorboard" \
+    --display-name="Vertex AI Custom Training"
+PROJECT_ID=$(gcloud config get-value core/project)
+gcloud projects add-iam-policy-binding $PROJECT_ID \
+    --member=serviceAccount:$SERVICE_ACCOUNT_ID@$PROJECT_ID.iam.gserviceaccount.com \
+    --role="roles/storage.admin"
+gcloud projects add-iam-policy-binding $PROJECT_ID \
+    --member=serviceAccount:$SERVICE_ACCOUNT_ID@$PROJECT_ID.iam.gserviceaccount.com \
+    --role="roles/bigquery.admin"
+gcloud projects add-iam-policy-binding $PROJECT_ID \
+    --member=serviceAccount:$SERVICE_ACCOUNT_ID@$PROJECT_ID.iam.gserviceaccount.com \
+    --role="roles/aiplatform.user"
