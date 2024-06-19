b3a46fde66c36bb93a173fefba2133c0be28029d
Wilson Jonathan Oey <61810752+wilsonoey60@users.noreply.github.com>
2024-06-19 16:13:30 +0800

    Update build_push_image.sh

--- build_push_image.sh
+++ build_push_image.sh
@@ -1,25 +1,25 @@
-export YOUR-BUCKET-NAME=gdntmmdmgdhmghdmgmdmmdmgdmghmghmdmdgmdg
+export YOURBUCKETNAME=1dntmmdmgdhmghdmgmdmmdmgdmghmghmdmdgmdg
 
 export REGION=us-central1
 
-gsutil mb gs://${YOUR-BUCKET-NAME}
+gsutil mb gs://${YOURBUCKETNAME}
 
 curl https://upload.wikimedia.org/wikipedia/commons/thumb/a/a4/Ada_Lovelace_portrait.jpg/800px-Ada_Lovelace_portrait.jpg --output ada.jpg
 
-gsutil cp ada.jpg gs://${YOUR-BUCKET-NAME}
+gsutil cp ada.jpg gs://${YOURBUCKETNAME}
 
 rm ada.jpg
 
-gsutil cp -r gs://${YOUR-BUCKET-NAME}/ada.jpg .
+gsutil cp -r gs://${YOURBUCKETNAME}/ada.jpg .
 
-gsutil cp gs://${YOUR-BUCKET-NAME}/ada.jpg gs://${YOUR-BUCKET-NAME}/image-folder/
+gsutil cp gs://${YOURBUCKETNAME}/ada.jpg gs://${YOURBUCKETNAME}/image-folder/
 
-gsutil ls gs://${YOUR-BUCKET-NAME}
+gsutil ls gs://${YOURBUCKETNAME}
 
-gsutil ls -l gs://${YOUR-BUCKET-NAME}/ada.jpg
+gsutil ls -l gs://${YOURBUCKETNAME}/ada.jpg
 
-gsutil acl ch -u AllUsers:R gs://${YOUR-BUCKET-NAME}/ada.jpg
+gsutil acl ch -u AllUsers:R gs://${YOURBUCKETNAME}/ada.jpg
 
-gsutil acl ch -d AllUsers gs://${YOUR-BUCKET-NAME}/ada.jpg
+gsutil acl ch -d AllUsers gs://${YOURBUCKETNAME}/ada.jpg
 
-gsutil rm gs://${YOUR-BUCKET-NAME}/ada.jpg
+gsutil rm gs://${YOURBUCKETNAME}/ada.jpg
