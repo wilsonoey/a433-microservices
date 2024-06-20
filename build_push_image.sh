25e71de8a71e9c44bb80a6979d677d8ac80c7999
Wilson Jonathan Oey <61810752+wilsonoey60@users.noreply.github.com>
2024-06-20 21:17:09 +0800

    Update build_push_image.sh

--- build_push_image.sh
+++ build_push_image.sh
@@ -1,54 +1,197 @@
-BLACK=`tput setaf 0`
-RED=`tput setaf 1`
-GREEN=`tput setaf 2`
-YELLOW=`tput setaf 3`
-BLUE=`tput setaf 4`
-MAGENTA=`tput setaf 5`
-CYAN=`tput setaf 6`
-WHITE=`tput setaf 7`
+gcloud services enable \
+  artifactregistry.googleapis.com \
+  cloudfunctions.googleapis.com \
+  cloudbuild.googleapis.com \
+  eventarc.googleapis.com \
+  run.googleapis.com \
+  logging.googleapis.com \
+  pubsub.googleapis.com
 
-BG_BLACK=`tput setab 0`
-BG_RED=`tput setab 1`
-BG_GREEN=`tput setab 2`
-BG_YELLOW=`tput setab 3`
-BG_BLUE=`tput setab 4`
-BG_MAGENTA=`tput setab 5`
-BG_CYAN=`tput setab 6`
-BG_WHITE=`tput setab 7`
 
-BOLD=`tput bold`
-RESET=`tput sgr0`
-#----------------------------------------------------start--------------------------------------------------#
+sleep 70
 
-echo "${YELLOW}${BOLD}Starting${RESET}" "${GREEN}${BOLD}Execution${RESET}"
 
-export REGION_1=us-east1
+PROJECT_NUMBER=$(gcloud projects describe $DEVSHELL_PROJECT_ID --format='value(projectNumber)')
 
-gcloud compute networks create managementnet --subnet-mode=custom
 
-gcloud compute networks subnets create managementsubnet-$REGION_1 --network=managementnet --region=$REGION_1 --range=10.130.0.0/20
+gcloud projects add-iam-policy-binding $DEVSHELL_PROJECT_ID \
+    --member=serviceAccount:$PROJECT_NUMBER-compute@developer.gserviceaccount.com \
+    --role=roles/eventarc.eventReceiver
 
-gcloud compute networks create privatenet --subnet-mode=custom
 
-gcloud compute networks subnets create privatesubnet-$REGION_1 --network=privatenet --region=$REGION_1 --range=172.16.0.0/24
+sleep 20
 
-gcloud compute networks subnets create privatesubnet-$REGION_2 --network=privatenet --region=$REGION_2 --range=172.20.0.0/20
+SERVICE_ACCOUNT="$(gsutil kms serviceaccount -p $DEVSHELL_PROJECT_ID)"
 
-gcloud compute firewall-rules create managementnet-allow-icmp-ssh-rdp --direction=INGRESS --priority=1000 --network=managementnet --action=ALLOW --rules=icmp,tcp:22,tcp:3389 --source-ranges=0.0.0.0/0
+gcloud projects add-iam-policy-binding $DEVSHELL_PROJECT_ID \
+    --member="serviceAccount:${SERVICE_ACCOUNT}" \
+    --role='roles/pubsub.publisher'
 
-gcloud compute firewall-rules create privatenet-allow-icmp-ssh-rdp --direction=INGRESS --priority=1000 --network=privatenet --action=ALLOW --rules=icmp,tcp:22,tcp:3389 --source-ranges=0.0.0.0/0
+sleep 20
 
-gcloud compute instances create	managementnet-${REGION_1}-vm --zone=$ZONE --machine-type=e2-micro --subnet=managementsubnet-$REGION_1
 
-gcloud compute instances create privatenet-${REGION_1}-vm --zone=$ZONE --machine-type=e2-micro --subnet=privatesubnet-$REGION_1
+gcloud projects add-iam-policy-binding $DEVSHELL_PROJECT_ID \
+    --member=serviceAccount:service-$PROJECT_NUMBER@gcp-sa-pubsub.iam.gserviceaccount.com \
+    --role=roles/iam.serviceAccountTokenCreator
 
-gcloud compute instances create vm-appliance \
---zone=$ZONE \
---machine-type=e2-standard-4 \
---network-interface=network-tier=PREMIUM,stack-type=IPV4_ONLY,subnet=privatesubnet-$REGION_1 \
---network-interface=network-tier=PREMIUM,stack-type=IPV4_ONLY,subnet=managementsubnet-$REGION_1 \
---network-interface=network-tier=PREMIUM,stack-type=IPV4_ONLY,subnet=mynetwork
+sleep 20
 
-echo "${RED}${BOLD}Congratulations${RESET}" "${WHITE}${BOLD}for${RESET}" "${GREEN}${BOLD}Completing the Lab !!!${RESET}"
 
-#-----------------------------------------------------end----------------------------------------------------------#
+gsutil mb -l $REGION gs://$DEVSHELL_PROJECT_ID-bucket
+
+gcloud pubsub topics create $TOPIC_NAME
+
+mkdir quicklab
+cd quicklab
+
+cat > index.js <<'EOF_END'
+const functions = require('@google-cloud/functions-framework');
+const crc32 = require("fast-crc32c");
+const { Storage } = require('@google-cloud/storage');
+const gcs = new Storage();
+const { PubSub } = require('@google-cloud/pubsub');
+const imagemagick = require("imagemagick-stream");
+
+functions.cloudEvent('$FUNCTION_NAME', cloudEvent => {
+  const event = cloudEvent.data;
+
+  console.log(`Event: ${event}`);
+  console.log(`Hello ${event.bucket}`);
+
+  const fileName = event.name;
+  const bucketName = event.bucket;
+  const size = "64x64"
+  const bucket = gcs.bucket(bucketName);
+  const topicName = "$TOPIC_NAME";
+  const pubsub = new PubSub();
+  if ( fileName.search("64x64_thumbnail") == -1 ){
+    // doesn't have a thumbnail, get the filename extension
+    var filename_split = fileName.split('.');
+    var filename_ext = filename_split[filename_split.length - 1];
+    var filename_without_ext = fileName.substring(0, fileName.length - filename_ext.length );
+    if (filename_ext.toLowerCase() == 'png' || filename_ext.toLowerCase() == 'jpg'){
+      // only support png and jpg at this point
+      console.log(`Processing Original: gs://${bucketName}/${fileName}`);
+      const gcsObject = bucket.file(fileName);
+      let newFilename = filename_without_ext + size + '_thumbnail.' + filename_ext;
+      let gcsNewObject = bucket.file(newFilename);
+      let srcStream = gcsObject.createReadStream();
+      let dstStream = gcsNewObject.createWriteStream();
+      let resize = imagemagick().resize(size).quality(90);
+      srcStream.pipe(resize).pipe(dstStream);
+      return new Promise((resolve, reject) => {
+        dstStream
+          .on("error", (err) => {
+            console.log(`Error: ${err}`);
+            reject(err);
+          })
+          .on("finish", () => {
+            console.log(`Success: ${fileName} → ${newFilename}`);
+              // set the content-type
+              gcsNewObject.setMetadata(
+              {
+                contentType: 'image/'+ filename_ext.toLowerCase()
+              }, function(err, apiResponse) {});
+              pubsub
+                .topic(topicName)
+                .publisher()
+                .publish(Buffer.from(newFilename))
+                .then(messageId => {
+                  console.log(`Message ${messageId} published.`);
+                })
+                .catch(err => {
+                  console.error('ERROR:', err);
+                });
+          });
+      });
+    }
+    else {
+      console.log(`gs://${bucketName}/${fileName} is not an image I can handle`);
+    }
+  }
+  else {
+    console.log(`gs://${bucketName}/${fileName} already has a thumbnail`);
+  }
+});
+EOF_END
+
+sed -i "8c\functions.cloudEvent('$FUNCTION_NAME', cloudEvent => { " index.js
+
+sed -i "18c\  const topicName = '$TOPIC_NAME';" index.js
+
+cat > package.json <<EOF_END
+{
+    "name": "thumbnails",
+    "version": "1.0.0",
+    "description": "Create Thumbnail of uploaded image",
+    "scripts": {
+      "start": "node index.js"
+    },
+    "dependencies": {
+      "@google-cloud/functions-framework": "^3.0.0",
+      "@google-cloud/pubsub": "^2.0.0",
+      "@google-cloud/storage": "^5.0.0",
+      "fast-crc32c": "1.0.4",
+      "imagemagick-stream": "4.1.1"
+    },
+    "devDependencies": {},
+    "engines": {
+      "node": ">=4.3.2"
+    }
+  }
+EOF_END
+
+
+
+PROJECT_ID=$(gcloud config get-value project)
+BUCKET_SERVICE_ACCOUNT="${PROJECT_ID}@${PROJECT_ID}.iam.gserviceaccount.com"
+
+gcloud projects add-iam-policy-binding $PROJECT_ID \
+  --member=serviceAccount:$BUCKET_SERVICE_ACCOUNT \
+  --role=roles/pubsub.publisher
+
+
+
+
+# Your existing deployment command
+deploy_function() {
+    gcloud functions deploy $FUNCTION_NAME \
+    --gen2 \
+    --runtime nodejs20 \
+    --trigger-resource $DEVSHELL_PROJECT_ID-bucket \
+    --trigger-event google.storage.object.finalize \
+    --entry-point $FUNCTION_NAME \
+    --region=$REGION \
+    --source . \
+    --quiet
+}
+
+# Variables
+SERVICE_NAME="$FUNCTION_NAME"
+
+# Loop until the Cloud Run service is created
+while true; do
+  # Run the deployment command
+  deploy_function
+
+  # Check if Cloud Run service is created
+  if gcloud run services describe $SERVICE_NAME --region $REGION &> /dev/null; then
+    echo "Cloud Run service is created. Exiting the loop."
+    break
+  else
+    echo "Waiting for Cloud Run service to be created..."
+    echo "Meantime Subscribe to Quicklab[https://www.youtube.com/@quick_lab]."
+    sleep 10
+  fi
+done
+
+
+
+
+curl -o map.jpg https://storage.googleapis.com/cloud-training/gsp315/map.jpg
+
+gsutil cp map.jpg gs://$DEVSHELL_PROJECT_ID-bucket/map.jpg
+
+gcloud projects remove-iam-policy-binding $DEVSHELL_PROJECT_ID \
+--member=user:$USERNAME2 \
+--role=roles/viewer
