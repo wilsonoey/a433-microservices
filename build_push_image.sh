4b5bab8268cb454bcbe3c06bd3094bbdcb01e200
Wilson Jonathan Oey <61810752+wilsonoey60@users.noreply.github.com>
2024-06-20 08:09:49 +0800

    Update build_push_image.sh

--- build_push_image.sh
+++ build_push_image.sh
@@ -1,24 +1,7 @@
-export ZONE=us-east4-b
-export REGION=us-east4
-
-gcloud config set project qwiklabs-gcp-00-8869f7b1bdf6
-
-gcloud dataproc clusters create example-cluster \
-  --region=$REGION \
-  --zone=$ZONE \
-  --master-machine-type=e2-standard-2 \
-  --master-boot-disk-size=50GB \
-  --num-workers=2 \
-  --worker-machine-type=e2-standard-2 \
-  --worker-boot-disk-size=50GB
-
+gcloud config set dataproc/region $REGION
+gcloud dataproc clusters create example-cluster --worker-boot-disk-size 500
 gcloud dataproc jobs submit spark \
-  --region=$REGION \
-  --cluster=example-cluster \
-  --class=org.apache.spark.examples.SparkPi \
-  --jars=file:///usr/lib/spark/examples/jars/spark-examples.jar \
-  -- 1000
-
-gcloud dataproc clusters update example-cluster \
-  --num-workers=4 \
-  --region=$REGION
+--cluster example-cluster \
+--class org.apache.spark.examples.SparkPi \
+--jars file:///usr/lib/spark/examples/jars/spark-examples.jar \
+-- 1000
