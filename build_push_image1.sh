6add92292fbcb0670fc77c4772edc139bd583d8c
Wilson Jonathan Oey <61810752+wilsonoey60@users.noreply.github.com>
2024-06-24 07:20:21 +0800

    Update build_push_image1.sh

--- build_push_image1.sh
+++ build_push_image1.sh
@@ -1,1 +1,15 @@
-python model.py
+gcloud services enable apikeys.googleapis.com
+export API_KEY=AIzaSyBRVaOyuAzDC56fJx2VnaBeCTY45uJPFDM
+touch request.json
+tee request.json <<EOF
+{
+  "document":{
+    "type":"PLAIN_TEXT",
+    "content":"Joanne Rowling, who writes under the pen names J. K. Rowling and Robert Galbraith, is a British novelist and screenwriter who wrote the Harry Potter fantasy series."
+  },
+  "encodingType":"UTF8"
+}
+EOF
+cat request.json
+curl "https://language.googleapis.com/v1/documents:analyzeEntities?key=${API_KEY}" -s -X POST -H "Content-Type: application/json" --data-binary @request.json > result.json
+cat result.json
