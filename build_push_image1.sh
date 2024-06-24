64b80e67cf3e9919c370f8c88af2056e55a6be0e
Wilson Jonathan Oey <61810752+wilsonoey60@users.noreply.github.com>
2024-06-24 16:35:30 +0800

    Update build_push_image1.sh

--- build_push_image1.sh
+++ build_push_image1.sh
@@ -1,17 +1,26 @@
-gcloud config set compute/zone $ZONE
+export DOCKER_TAG=gcr.io/ql-shared-resources-test/resistance_solution@sha256:d9095cbd6f7ca69b1a30c58c4272b68062d2004ed259ff0dcb9af0ceb92b393b
+export VISERVING_CPU_DOCKER_WITH_MODEL=${DOCKER_TAG}
+export HTTP_PORT=8602
+export LOCAL_METRIC_PORT=8603
 
-export PROJECT_ID=$(gcloud info --format='value(config.project)')
+docker pull ${VISERVING_CPU_DOCKER_WITH_MODEL}
 
-gcloud container clusters get-credentials central --zone $ZONE
+docker run -v /secrets:/secrets --rm -d --name "test_cpu" \
+--network="host" \
+-p ${HTTP_PORT}:8602 \
+-p ${LOCAL_METRIC_PORT}:8603 \
+-t ${VISERVING_CPU_DOCKER_WITH_MODEL} \
+--metric_project_id="${PROJECT_ID}" \
+--use_default_credentials=false \
+--service_account_credentials_json=/secrets/assembly-usage-reporter.json
 
-git clone https://github.com/xiangshen-dk/microservices-demo.git
+docker container ls
 
-cd microservices-demo
+gsutil cp gs://cloud-training/gsp895/prediction_script.py .
 
-kubectl apply -f release/kubernetes-manifests.yaml
+export PROJECT_ID=$(gcloud config get-value core/project)
+gsutil mb gs://${PROJECT_ID}
+gsutil -m cp gs://cloud-training/gsp895/pcb_images/*.png \
+gs://${PROJECT_ID}/demo_pcb_images/
+gsutil cp gs://${PROJECT_ID}/demo_pcb_images/image_275_cx98_cy16_r-5.png .
 
-sleep 30
-
-gcloud logging metrics create Error_Rate_SLI \
-  --description="subscribe to quicklab" \
-  --log-filter="resource.type=\"k8s_container\" severity=ERROR labels.\"k8s-pod/app\": \"recommendationservice\"" 
